4e610778ba7dac452a514e82c061eb27e116be9f
Ranielli Montagna <raniellimontagna@hotmail.com>
2022-11-22 22:05:33 -0300

    feat: cria repositories iniciais para conexão com o banco

---
+++ lib/repository/eventos_repository.dart
@@ -0,0 +1,54 @@
+import 'package:mygoodtrip/services/database_service/database_service.dart';
+import 'package:sqflite/sqflite.dart';
+
+class ViagensRepository {
+  static const nameDatabase = 'eventos';
+
+  Future<Map?> getEventos() async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      List<Map> result = await db.rawQuery('SELECT * FROM $nameDatabase');
+      if (result.isNotEmpty) {
+        return result[0];
+      } else {
+        return null;
+      }
+    } catch (err) {
+      throw Exception('Ocorreu algum erro ao buscar as questões. Motivo: $err');
+    }
+  }
+
+  Future<void> insertEventos(Map eventos) async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      // await db.insert(nameDatabase, eventos);
+    } catch (err) {
+      throw Exception(
+          'Ocorreu algum erro ao inserir as questões. Motivo: $err');
+    }
+  }
+
+  Future<void> updateEventos(Map eventos) async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      // await db.update(nameDatabase, eventos);
+    } catch (err) {
+      throw Exception(
+          'Ocorreu algum erro ao atualizar as questões. Motivo: $err');
+    }
+  }
+
+  Future<void> deleteEventos(int id) async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      await db.rawQuery('DELETE FROM $nameDatabase WHERE id = $id');
+    } catch (err) {
+      throw Exception(
+          'Ocorreu algum erro ao deletar as questões. Motivo: $err');
+    }
+  }
+}
---
+++ lib/repository/viagens_repository.dart
@@ -0,0 +1,54 @@
+import 'package:mygoodtrip/services/database_service/database_service.dart';
+import 'package:sqflite/sqflite.dart';
+
+class ViagensRepository {
+  static const nameDatabase = 'viagens';
+
+  Future<Map?> getViagens() async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      List<Map> result = await db.rawQuery('SELECT * FROM $nameDatabase');
+      if (result.isNotEmpty) {
+        return result[0];
+      } else {
+        return null;
+      }
+    } catch (err) {
+      throw Exception('Ocorreu algum erro ao buscar as questões. Motivo: $err');
+    }
+  }
+
+  Future<void> insertViagens(Map viagens) async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      // await db.insert(nameDatabase, viagens);
+    } catch (err) {
+      throw Exception(
+          'Ocorreu algum erro ao inserir as questões. Motivo: $err');
+    }
+  }
+
+  Future<void> updateViagens(Map viagens) async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      // await db.update(nameDatabase, viagens);
+    } catch (err) {
+      throw Exception(
+          'Ocorreu algum erro ao atualizar as questões. Motivo: $err');
+    }
+  }
+
+  Future<void> deleteViagens(int id) async {
+    Database db = await DatabaseService.datebase;
+
+    try {
+      await db.rawQuery('DELETE FROM $nameDatabase WHERE id = $id');
+    } catch (err) {
+      throw Exception(
+          'Ocorreu algum erro ao deletar as questões. Motivo: $err');
+    }
+  }
+}
